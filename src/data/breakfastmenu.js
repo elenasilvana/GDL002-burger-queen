2ccdc073ae6b9f4c3496e03f0eda6d300825b964
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-05-20 22:21:32 -0500

    se agregó directorio data con data de menus

---
+++ src/data/breakfastmenu.js
@@ -0,0 +1,11 @@
+export const BreakfastMenu = [
+{product: "Café de olla",
+price: 5,
+img: 'https://i.postimg.cc/0Q2hbVqf/cafe-de-olla.jpg'},
+{product: "Capuccino latte",
+price: 7, img: 'https://i.postimg.cc/j5gmNyxr/capuchino.jpg' },
+{product: "Jugos de fruta",
+price: 7, img: 'https://i.postimg.cc/BbRz7sPY/jugo.jpg' },
+{product: "Sandwich de calabacita", 
+price: 10, img: 'https://i.postimg.cc/8Pr9JFxq/portada-sandwich-verde.jpg' }
+];
---
+++ src/data/normalmenu.js
@@ -0,0 +1,19 @@
+export const NormalMenu = [
+{product: "Hamburguesa Simple",
+price: 10,
+img: 'https://i.postimg.cc/6QBmmMTX/burger-simple.jpg'},
+{product: "Hamburguesa Doble",
+price: 15, img: 'https://i.postimg.cc/FHy6pMrp/burger-double.jpg'},
+{product: "Papas fritas",
+price: 5, img: 'https://i.postimg.cc/fyVFvkyS/frenchfries.jpg'},
+{product: "Aros de Cebolla", 
+price: 5, img: 'https://i.postimg.cc/XNPtYK1f/onion-rings.jpg'},
+{product: "Agua 500ml",
+price: 5, img: 'https://i.postimg.cc/DzctvtR0/water.jpg'},
+{product: "Agua 750ml",
+price: 7, img: "https://i.postimg.cc/DzctvtR0/water.jpg"},
+{product: "Refresco 500ml",
+price: 7, img: "https://i.postimg.cc/0yR3pksr/150617-sodaglass-stock.jpg"},
+{product: "Refresco 750ml",
+price: 10, img: "https://i.postimg.cc/0yR3pksr/150617-sodaglass-stock.jpg"}
+];
