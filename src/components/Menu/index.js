4a8bc9910f472458e519489202860ec3998cbe7e
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-05-20 22:26:50 -0500

    se agregó contenido a menu, y contenido temporal a order

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,7 +1,27 @@
 import React from 'react';
-const Menu = () => (
-<div>
-<h1>Menu</h1>
-</div>
-);
+import {NormalMenu} from '../../data/normalmenu';
+
+
+const Menu = (props) => { 
+
+	let normal = NormalMenu.map((product) => {
+
+	return (
+		<div className="cardFood-container">
+			<a>
+				<div className="food-image" style={{backgroundImage: "url(" + product.img + ")"}}></div>
+			</a>
+			<h3> {product.product}</h3>
+			<h2>${product.price}</h2>
+		</div>
+		) 
+	});
+
+	return (
+		<div className="container">
+			{normal}
+		</div>
+
+	)
+};
 export default Menu;
--- src/components/Order/index.js
+++ src/components/Order/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
-const Order = () => (
-<div>
-<h1>Order</h1>
-</div>
-);
+import {BreakfastMenu} from '../../data/breakfastmenu';
+
+
+const Order = (props) => { 
+
+	let breackfast = BreakfastMenu.map((product) => {
+
+	return (
+		<div className="cardFood-container">
+			<a>
+				<div className="food-image" style={{backgroundImage: "url(" + product.img + ")"}}></div>
+			</a>
+			<h3> {product.product}</h3>
+			<h2>${product.price}</h2>
+		</div>
+		) 
+	});
+
+	return (
+		<div className="container">
+			{breackfast}
+		</div>
+
+	)
+};
+
 export default Order;
