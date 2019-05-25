132cbf051ff7d4e8c34df03780fd06e979c1e4bc
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-05-25 15:36:13 -0500

    se agregaron propiedades id, opciones y extra a los objetos de menu en data

--- src/data/breakfastmenu.js
+++ src/data/breakfastmenu.js
@@ -1,11 +1,33 @@
 export const BreakfastMenu = [
-{product: "Café de olla",
+{
+product: "Café de olla",
 price: 5,
-img: 'https://i.postimg.cc/0Q2hbVqf/cafe-de-olla.jpg'},
-{product: "Capuccino latte",
-price: 7, img: 'https://i.postimg.cc/j5gmNyxr/capuchino.jpg' },
-{product: "Jugos de fruta",
-price: 7, img: 'https://i.postimg.cc/BbRz7sPY/jugo.jpg' },
-{product: "Sandwich de calabacita", 
-price: 10, img: 'https://i.postimg.cc/8Pr9JFxq/portada-sandwich-verde.jpg' }
+img: 'https://i.postimg.cc/0Q2hbVqf/cafe-de-olla.jpg',
+options: null,
+extras: null,
+id: 1
+},
+{
+product: "Capuccino latte",
+price: 7, 
+img: 'https://i.postimg.cc/j5gmNyxr/capuchino.jpg', 
+options: null,
+extras: null,
+id: 2 
+},
+{
+product: "Jugos de fruta",
+price: 7, img: 'https://i.postimg.cc/BbRz7sPY/jugo.jpg', 
+options: null,
+extras: null,
+id: 3 
+},
+{
+product: "Sandwich de calabacita", 
+price: 10, 
+img: 'https://i.postimg.cc/8Pr9JFxq/portada-sandwich-verde.jpg',
+options: null,
+extras: null,
+id: 4 
+}
 ];
