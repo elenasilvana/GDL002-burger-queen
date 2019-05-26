ff58a27b667e36a646e74e40a4b7d7ea4b66148b
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-05-26 18:59:36 -0500

    se implementó tabla de reactstrap así como los props necesarios para llenar la tabla de manera dinámica

--- src/components/Comanda.js
+++ src/components/Comanda.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Buttons from './Button';
+import Table from 'react-bootstrap/Table'
 
 //aquí debería estar el componente de la comanda 
 
@@ -7,7 +8,29 @@ class Comanda extends Component {
 	render(){
 		return(
 			<div className="comanda-container col-md-6">
-				<h1>aqui aparecerá la comanda</h1>
+			<Table responsive striped bordered hover>
+			  <thead>
+			    <tr>
+			      <th>#</th>
+			      <th>Comida</th>
+			      <th>Precio</th>
+			      <th>boton</th>
+			    </tr>
+			  </thead>
+			  <tbody>
+			  { 
+			  	this.props.order.map((line, index) => (
+				    <tr>
+				      <td>no. index</td>
+				      <td>{line.product}</td>
+				      <td>${line.price}</td>
+				      <td>boton de borrar</td>
+				    </tr>
+
+			  	))}
+
+			  </tbody>
+			</Table>
 				<Buttons action={()=>{console.log('boton de la orden')}} name={'Enviar Orden'} />
 			</div>
 			)
