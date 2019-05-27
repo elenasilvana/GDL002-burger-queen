2e42160d6e6173d4194c7a7a1adcb75b5f918476
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-05-26 19:01:56 -0500

    se habilitó funcionalidad y se borró codigo muerto

--- src/components/ShowMenu.js
+++ src/components/ShowMenu.js
@@ -1,17 +1,10 @@
 import React, {Component} from 'react';
 import Button from 'react-bootstrap/Button';
-
 //import Buttons from './Button';
 
 class ShowMenu extends Component {
-	//le paso la función por ejemplo: 
-	// this.props.nombredelafunción(this.props.argumento)
-	//product, price, key
 
-
-//<Buttons action={()=>this.props.addItem(this.props.product, this.props.price, this.props.key)} name={this.props.product} />
-// componente boton <Buttons action={()=>this.props.addItem(this.props.product, this.props.price)} name={this.props.product} />
-	render(){
+render(){
 		//console.log('aqui los props', this.props);
 
 		return (
@@ -19,17 +12,17 @@ class ShowMenu extends Component {
 				<a>
 					<div className="food-image" style={{backgroundImage: "url(" + this.props.img + ")"}}></div>
 				</a>
-				<Button variant="outline-secondary" size="lg" 
-				onClick={()=>this.action}
-				//onClick={()=>console.log(this.props.product, this.props.price)}
-				>
-				{this.props.product}
-				</Button>
+				<button 
+				onClick={this.props.addItem}
+				data-price={this.props.price}
+				>{this.props.product}</button>
 				<h2>${this.props.price}</h2>
+				<h3>Index {this.props.key}</h3>
 			</div>
 			
 		);
 	}
+
 }
 
 export default ShowMenu;
