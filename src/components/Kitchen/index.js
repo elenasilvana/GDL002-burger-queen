c06e482f86bafaefb02bc2a6fda427b5fcec4b40
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-07-17 12:54:59 -0500

    Kitchen component has been created and show orders from DB

--- src/components/Kitchen/index.js
+++ src/components/Kitchen/index.js
@@ -1,10 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ShowOrders from '../ShowOrders';
+
+
 
 class Kitchen extends Component {
-    render(){
-        return(
-            <h1>aquí Kitchen</h1>
+    constructor() {
+        super();
+        this.state = {
+            orders: []
+        }
+    }
 
+
+    componentDidMount() {
+        const url = "http://localhost:8080/order";
+
+        fetch(url)
+            .then(res => res.json())
+            .then((orderAsJson) => { this.setState({ orders: orderAsJson }, console.log('aquí las ordenes', orderAsJson)) })
+    }
+
+    render() {
+       // console.log(this.state.orders);
+        return (
+            <div>
+                <h1>aquí Kitchen / show orders</h1>
+
+                {this.state.orders.orders ? this.state.orders.orders.map((order, index)=>(
+                    <ShowOrders 
+                    
+                    name={order.clientname}
+                    items={order.items}
+                    status={order.status}
+                    />
+                ))
+                : console.log('waiting for orders response')}
+
+            </div>
         );
     }
 
