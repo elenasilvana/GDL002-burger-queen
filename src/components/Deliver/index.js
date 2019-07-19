fa4a2d7078d79dd1ca72459ad43f34363d75f286
Elena Silvana <elenasilvana@pop-os.localdomain>
2019-07-18 23:05:02 -0500

    Deliver file created

---
+++ src/components/Deliver/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+class Deliver extends Component {
+    render(){
+        return(
+            <h2>Entregas</h2>
+        )
+    }
+}
+
+export default Deliver;
